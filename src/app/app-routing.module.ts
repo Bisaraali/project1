d2a8a80f09bb0c5707ee588eb5976147a6438a2a
unknown <you@example.com>
2023-06-20 09:58:32 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgetComponent } from './forget/forget.component';
+import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
+import { ChildrenComponent } from './children/children.component';
+import { HomeComponent } from './home/home.component';
+import { VaccinationComponent } from './vaccination/vaccination.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  
+  {path:"reg",component:RegisterComponent},
+  {
+    path:"forget",component:ForgetComponent },
+    {path:"Dashboard",component:DashboardComponent,
+    children:[
+      {path:"",component:HomeComponent},
+      {path:"Children",component:ChildrenComponent},
+      {path:"Vaccination",component:VaccinationComponent},
+      {path:"Doctor",component:DoctorComponent}
+    ]
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgetComponent } from './forget/forget.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { ChildrenComponent } from './children/children.component';
+import { VaccinationComponent } from './vaccination/vaccination.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgetComponent,
+    DashboardComponent,
+    HomeComponent,
+    ChildrenComponent,
+    VaccinationComponent,
+    DoctorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
